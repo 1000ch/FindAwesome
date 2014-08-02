114367d7f3838096f1f5c92f887b29ce1e0181eb
1000ch <shogo.sensui@gmail.com>
2014-08-02 12:20:00 +0900

    wip

--- public/js/main.js
+++ public/js/main.js
@@ -5,8 +5,34 @@ $(function () {
     console.log(imageData);
   });
 
-  $.get('/fonts/FontAwesome.otf').done(function (data) {
-    var jdataview = new jDataView(data);
-    console.log(jdataview); 
-  });
+  var $fontawesome = $('#js-fontawesome');
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('get', '/fonts/fontawesome-webfont.ttf', true);
+  xhr.overrideMimeType('font/opentype');
+  xhr.responseType = 'arraybuffer';
+  
+  xhr.onreadystatechange = function (e) {
+    if (this.readyState === 4 && this.status === 200) {
+
+      var fa = new ttfjs.TTF(e.target.response);
+      var scale = 1000 / fa.head.unitsPerEm;
+
+      var html = '';
+      var path = '';
+      var glyf = null;
+
+      for (var i = 0, l = fa.glyf.length;i < l;i++) {
+        glyf = fa.glyf[i];
+        if (glyf.path !== '') {
+          path = '<path d="' + glyf.path + '" transform="scale(' + scale + ', ' + scale + ')">' + '</path>';
+          html += '<li><svg width="100" height="100" viewBox="0 0 100 100"><g>' + path + '</g></svg></li>'; 
+        }
+      }
+
+      $fontawesome.append(html);
+    }
+  };
+  
+  xhr.send();
 });
