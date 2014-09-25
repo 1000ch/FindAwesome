bb40248a74ede6578de215019c272c37a339e2de
1000ch <shogo.sensui@gmail.com>
2014-09-25 18:04:23 +0900

    add check logic

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,8 @@ $(function () {
   canvas.onThumbnail.addListener(function (imageData) {
     var font;
     var matches;
+    var maxMatches = 0;
+    var maxMatchesSelector = '';
     for (var i = 0, l = fonts.length;i < l;i++) {
       matches = 0;
       font = fonts[i];
@@ -14,43 +16,62 @@ $(function () {
           matches++;
         }
       }
-      console.log(matches);
+      if (maxMatches < matches) {
+        maxMatchesSelector = font.selector;
+      }
     }
+    $('#js-fontawesome').find('li').each(function () {
+      var $this = $(this);
+      if ($this.find('.js-selector').text() === maxMatchesSelector) {
+        $this.show();
+      } else {
+        $this.hide();
+      }
+    });
   });
+
+});
+
+$(window).on('load', function () {
 
   var $fontawesome = $('#js-fontawesome');
 
-  $.ajax({
-    url: '/api/fonts',
-    method: 'get'
-  }).done(function (data) {
-    
-    var html = '';
-    data.fonts.forEach(function (font) {
-      html += '<li>';
-      html += '<span class="js-selector">' + font.selector + '</span>';
-      html += '<span style="display: none;" class="js-content">' + font.content.replace(/\"/g, '') + '</span>';
-      html += '<canvas class="js-canvas" width="32" height="32"></canvas>';
-      html += '</li>';
-    });
-    $fontawesome.html(html);
+  document.fonts.ready().then(function (fontFaceSet) {
 
-    $fontawesome.find('li').each(function () {
-      var $this = $(this);
-      var canvas = $this.find('.js-canvas').get(0);
+    if (fontFaceSet.check('24px "FontAwesome"')) {
+      $.ajax({
+        url: '/api/fonts',
+        method: 'get'
+      }).done(function (data) {
 
-      var selector = $this.find('.js-selector').text();
-      var content = $this.find('.js-content').text().replace(/\\/, '0x');
+        var html = '';
+        data.fonts.forEach(function (font) {
+          html += '<li>';
+          html += '<span class="js-selector">' + font.selector + '</span>';
+          html += '<span style="display: none;" class="js-content">' + font.content.replace(/\"/g, '') + '</span>';
+          html += '<canvas class="js-canvas" width="32" height="32"></canvas>';
+          html += '</li>';
+        });
+        $fontawesome.html(html);
 
-      var ctx = canvas.getContext('2d');
-      ctx.font = '24px FontAwesome';
-      ctx.textAlign = 'center';
-      ctx.textBaseline = 'middle';
-      ctx.fillText(String.fromCharCode(content), canvas.width / 2, canvas.height / 2);
-      fonts.push({
-        selector: selector,
-        imageData: ctx.getImageData(0, 0, canvas.width, canvas.height)
+        $fontawesome.find('li').each(function () {
+          var $this = $(this);
+          var canvas = $this.find('.js-canvas').get(0);
+
+          var selector = $this.find('.js-selector').text();
+          var content = $this.find('.js-content').text().replace(/\\/, '0x');
+
+          var ctx = canvas.getContext('2d');
+          ctx.font = '24px FontAwesome';
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'middle';
+          ctx.fillText(String.fromCharCode(content), canvas.width / 2, canvas.height / 2);
+          fonts.push({
+            selector: selector,
+            imageData: ctx.getImageData(0, 0, canvas.width, canvas.height)
+          });
+        });
       });
-    });
+    }
   });
 });
